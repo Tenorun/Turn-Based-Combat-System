95397062dd3ccae1563960c9d6c5c99cb5513b1a
Tenorun <105449250+Tenorun@users.noreply.github.com>
2023-11-27 22:08:51 +0900

    아이템 페이지 만들기2

---
+++ Assets/Scripts/ItemMenuPage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemMenuPage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite[] PageDigit;
+    public Sprite[] InventorySizeDigit;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
