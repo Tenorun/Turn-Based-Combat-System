bca388a0f8ef266f47c91500f6ead12a67c76f53
Tenorun <105449250+Tenorun@users.noreply.github.com>
2023-12-21 14:53:28 +0900

    아이템 실행기 기능 구현2, 다음은 인벤토리를 구현할것.

--- Assets/Scripts/ItemExecuter.cs
+++ Assets/Scripts/ItemExecuter.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class ItemExecuter : MonoBehaviour
 {
     public GameObject ItemMaster;
+    public GameObject Pointer;
+    public GameObject Page;
 
     int currentPage;
     int itemPointerNum;
 
 
+    //테스트용 인벤토리
+    int[,] testReadInventory = new int[,] { { 1, 2, 3, 4, 5 }, { 1, 1, 1, 1, 1 }, { 2, 2, 2, 2, 2 }, { 3, 3, 3, 3, 3 }, { 5, 5, 5, 5, 5 } };
+
     // Update is called once per frame
     void Update()
     {
-        itemPointerNum = ItemMaster.GetComponent<ItemMenuPointer>().itemPointerNum;
-        currentPage = ItemMaster.GetComponent<ItemMenuPage>().currentPage;
+        itemPointerNum = Pointer.GetComponent<ItemMenuPointer>().itemPointerNum;
+        currentPage = Page.GetComponent<ItemMenuPage>().currentPage;
 
         if (Input.GetButtonDown("Submit"))
         {
+            ItemMaster.GetComponent<ItemData>().UseItemEffect(testReadInventory[currentPage - 1, itemPointerNum]);
             //TODO: 플레이어 인벤토리에서 화살표가 올라가져있는 아이템 실행 스크립트 작성
         }
     }
